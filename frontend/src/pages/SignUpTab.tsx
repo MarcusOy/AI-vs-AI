01f0e26e4d7330346c24273f0c9852dd8730fc7a
QihaoGe <ge69@purdue.edu>
2022-09-18 11:47:52 -0400

    implement data passing for hook in signup form

--- frontend/src/pages/SignUpTab.tsx
+++ frontend/src/pages/SignUpTab.tsx
@@ -1,74 +1,104 @@
-import React from 'react'
-import {
-    Input,
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Button,
-    ButtonGroup,
-    Container,
-    Center,
-    Heading,
-} from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+import { Input, Flex, Box, FormControl, FormLabel, Button, Center, Heading } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const SignUpTab = () => {
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const onClick = () => {
+        fetch('https://webhook.site/f87e51c8-9a3a-4836-8603-6047d18985e6', {
+            body: JSON.stringify({
+                firstname,
+                lastname,
+                email,
+                username,
+                password,
+            }),
+            method: 'POST',
+        })
+    }
     return (
         <Flex>
             <Box>
-                <SignupHeader />
-                <SignUpForm />
+                <Center>
+                    <Box>
+                        <Heading>Welcome, AI Creators</Heading>
+                    </Box>
+                </Center>
+                <Box>
+                    <form>
+                        <FormControl>
+                            <FormLabel>First Name</FormLabel>
+                            <Input
+                                type='firstname'
+                                placeholder='Enter your first name'
+                                value={firstname}
+                                onChange={(e) => {
+                                    setFirstname(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Last Name</FormLabel>
+                            <Input
+                                type='lastname'
+                                placeholder='Enter your last name'
+                                value={lastname}
+                                onChange={(e) => {
+                                    setLastname(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                type='email'
+                                placeholder='Enter your email address'
+                                value={email}
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Username</FormLabel>
+                            <Input
+                                type='username'
+                                placeholder='Enter your username'
+                                value={username}
+                                onChange={(e) => {
+                                    setUsername(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                type='password'
+                                placeholder='Enter your password'
+                                value={password}
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Password-Confirm</FormLabel>
+                            <Input type='password' placeholder='Re-enter your password' />
+                        </FormControl>
+                    </form>
+                    <Center>
+                        <Button size='lg' colorScheme='cyan' mt='24px' onClick={onClick}>
+                            <Link to='/'>Complete Sign Up</Link> to mainpage
+                        </Button>
+                    </Center>
+                </Box>
             </Box>
         </Flex>
-    )
-}
-
-const SignUpForm = () => {
-    return (
-        <Box>
-            <form>
-                <FormControl>
-                    <FormLabel>First Name</FormLabel>
-                    <Input type='first name' placeholder='Enter your first name' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Last Name</FormLabel>
-                    <Input type='last name' placeholder='Enter your last name' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Email</FormLabel>
-                    <Input type='email' placeholder='Enter your email address' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Username</FormLabel>
-                    <Input type='username' placeholder='Enter your username' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Password</FormLabel>
-                    <Input type='password' placeholder='Enter your password' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Password-Confirm</FormLabel>
-                    <Input type='password' placeholder='Re-enter your password' />
-                </FormControl>
-            </form>
-            <Center>
-                <Button size='lg' colorScheme='cyan' mt='24px'>
-                    <Link to='/'>Complete Sign Up</Link> {/* to mainpage */}
-                </Button>
-            </Center>
-        </Box>
-    )
-}
-
-const SignupHeader = () => {
-    return (
-        <Center>
-            <Box>
-                <Heading>Welcome, AI Creators</Heading>
-            </Box>
-        </Center>
     )
 }
 
