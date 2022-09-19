66ae97e540e4573f63902cbaa201cc2e5ba2a271
afabisiak0427 <afabisia@purdue.edu>
2022-09-19 15:42:02 -0400

    Created the BugController.cs class to handle the sending of bug objects from client to server

--- backend/Controllers/AccountController.cs
+++ backend/Controllers/AccountController.cs
@@ -10,8 +10,8 @@ public class AccountController : Controller
         // The incoming message will be a new user object to add to the database
         
         // TODO - Add the new user to the database
-        
-        return Ok("User is added")
+
+        return Ok("User is added");
     }
 
     [Route("/Login")]
@@ -20,7 +20,7 @@ public class AccountController : Controller
         // As of right now, the incoming message will be a user object with their populated fields. This may change
         
         // TODO - Check the username and password against the database, and send appropriate response
-        
-        return Ok("Login is confirmed")
+
+        return Ok("Login is confirmed");
     }
 }
