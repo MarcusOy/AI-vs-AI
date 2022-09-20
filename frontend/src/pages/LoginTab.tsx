78ca26ced53e5b95112ea149b6fa198c46b84420
QihaoGe <ge69@purdue.edu>
2022-09-20 13:19:31 -0400

    some upgrade

--- frontend/src/pages/LoginTab.tsx
+++ frontend/src/pages/LoginTab.tsx
@@ -1,50 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Flex, FormControl, FormLabel, Input, Center, Heading, Button } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 const LoginTab = () => {
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const onClick = () => {
+        fetch('https://webhook.site/f87e51c8-9a3a-4836-8603-6047d18985e6', {
+            body: JSON.stringify({
+                email,
+                username,
+                password,
+            }),
+            method: 'POST',
+        })
+    }
     return (
         <Flex>
             <Box>
-                <LoginHeader />
-                <LoginForm />
+                <Center>
+                    <Box>
+                        <Heading color=''>Glad to see you</Heading>
+                    </Box>
+                </Center>
+                <Box>
+                    <form>
+                        <FormControl>
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                type='email'
+                                placeholder='Enter your email address'
+                                value={email}
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Username</FormLabel>
+                            <Input
+                                type='username'
+                                placeholder='Enter your username'
+                                value={username}
+                                onChange={(e) => {
+                                    setUsername(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                type='password'
+                                placeholder='Enter your password'
+                                value={password}
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                }}
+                            />
+                        </FormControl>
+                    </form>
+                    <Center>
+                        <Button size='lg' colorScheme='cyan' mt='24px' onClick={onClick}>
+                            <Link to='/'>Log In</Link> {/* to mainpage */}
+                        </Button>
+                    </Center>
+                </Box>
             </Box>
         </Flex>
-    )
-}
-
-const LoginForm = () => {
-    return (
-        <Box>
-            <form>
-                <FormControl>
-                    <FormLabel>Email</FormLabel>
-                    <Input type='email' placeholder='Enter your email address' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Username</FormLabel>
-                    <Input type='username' placeholder='Enter your username' />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Password</FormLabel>
-                    <Input type='password' placeholder='Enter your password' />
-                </FormControl>
-            </form>
-            <Center>
-                <Button size='lg' colorScheme='cyan' mt='24px'>
-                    <Link to='/'>Log In</Link> {/* to mainpage */}
-                </Button>
-            </Center>
-        </Box>
-    )
-}
-
-const LoginHeader = () => {
-    return (
-        <Center>
-            <Box>
-                <Heading color=''>Glad to see you</Heading>
-            </Box>
-        </Center>
     )
 }
 
--- frontend/src/pages/SignUpTab.tsx
+++ frontend/src/pages/SignUpTab.tsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 
-import { Input, Flex, Box, FormControl, FormLabel, Button, Center, Heading } from '@chakra-ui/react'
+import {
+    Input,
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Button,
+    Center,
+    Heading,
+    FormErrorMessage,
+    FormHelperText,
+    Alert,
+    AlertIcon,
+} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const SignUpTab = () => {
@@ -10,7 +23,14 @@ const SignUpTab = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [password2, setPassword2] = useState('')
-    const [error, setError] = useState(false)
+
+    const isError1 = firstname === ''
+    const isError2 = lastname === ''
+    const isError3 = email === ''
+    const isError4 = username === ''
+    const isError5 = password === ''
+    const isError6 = password2 === ''
+
     const onClick = () => {
         fetch('https://webhook.site/f87e51c8-9a3a-4836-8603-6047d18985e6', {
             body: JSON.stringify({
@@ -24,20 +44,6 @@ const SignUpTab = () => {
         })
     }
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     if (
-    //         firstname.length == 0 ||
-    //         lastname.length == 0 ||
-    //         email.length == 0 ||
-    //         username.length == 0 ||
-    //         password.length == 0 ||
-    //         password2.length == 0
-    //     ) {
-    //         setError(true)
-    //     }
-    // }
-
     return (
         <Flex>
             <Box>
@@ -48,7 +54,7 @@ const SignUpTab = () => {
                 </Center>
                 <Box>
                     <form>
-                        <FormControl>
+                        <FormControl isInvalid={isError1}>
                             <FormLabel>First Name</FormLabel>
                             <Input
                                 type='firstname'
@@ -58,8 +64,13 @@ const SignUpTab = () => {
                                     setFirstname(e.target.value)
                                 }}
                             />
+                            {!isError1 ? (
+                                <FormHelperText></FormHelperText>
+                            ) : (
+                                <FormErrorMessage>First name is required</FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={isError2}>
                             <FormLabel>Last Name</FormLabel>
                             <Input
                                 type='lastname'
@@ -69,8 +80,13 @@ const SignUpTab = () => {
                                     setLastname(e.target.value)
                                 }}
                             />
+                            {!isError2 ? (
+                                <FormHelperText></FormHelperText>
+                            ) : (
+                                <FormErrorMessage>Last name is required</FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={isError3}>
                             <FormLabel>Email</FormLabel>
                             <Input
                                 type='email'
@@ -80,8 +96,13 @@ const SignUpTab = () => {
                                     setEmail(e.target.value)
                                 }}
                             />
+                            {!isError3 ? (
+                                <FormHelperText></FormHelperText>
+                            ) : (
+                                <FormErrorMessage>Email is required</FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={isError4}>
                             <FormLabel>Username</FormLabel>
                             <Input
                                 type='username'
@@ -91,8 +112,13 @@ const SignUpTab = () => {
                                     setUsername(e.target.value)
                                 }}
                             />
+                            {!isError4 ? (
+                                <FormHelperText>Username </FormHelperText>
+                            ) : (
+                                <FormErrorMessage>Username is required</FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={isError5}>
                             <FormLabel>Password</FormLabel>
                             <Input
                                 type='password'
@@ -102,8 +128,15 @@ const SignUpTab = () => {
                                     setPassword(e.target.value)
                                 }}
                             />
+                            {!isError5 ? (
+                                <FormHelperText></FormHelperText>
+                            ) : (
+                                <FormErrorMessage>
+                                    Password is required. Length: 4-16
+                                </FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={isError6}>
                             <FormLabel>Password-Confirm</FormLabel>
                             <Input
                                 type='password'
@@ -113,11 +146,16 @@ const SignUpTab = () => {
                                     setPassword2(e.target.value)
                                 }}
                             />
+                            {!isError6 ? (
+                                <FormHelperText>Please match the password</FormHelperText>
+                            ) : (
+                                <FormErrorMessage>Password re-entry is required</FormErrorMessage>
+                            )}
                         </FormControl>
                     </form>
                     <Center>
                         <Button size='lg' colorScheme='cyan' mt='24px' onClick={onClick}>
-                            <Link to='/'>Complete Sign Up</Link>
+                            Complete Sign Up
                         </Button>
                     </Center>
                 </Box>
