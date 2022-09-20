8df4d789e82ce7b5c789c7a80c586a08d90826fd
QihaoGe <ge69@purdue.edu>
2022-09-20 13:18:14 -0400

    some upgrade

--- frontend/src/pages/SignUpTab.tsx
+++ frontend/src/pages/SignUpTab.tsx
@@ -9,6 +9,8 @@ const SignUpTab = () => {
     const [email, setEmail] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [error, setError] = useState(false)
     const onClick = () => {
         fetch('https://webhook.site/f87e51c8-9a3a-4836-8603-6047d18985e6', {
             body: JSON.stringify({
@@ -21,6 +23,21 @@ const SignUpTab = () => {
             method: 'POST',
         })
     }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     if (
+    //         firstname.length == 0 ||
+    //         lastname.length == 0 ||
+    //         email.length == 0 ||
+    //         username.length == 0 ||
+    //         password.length == 0 ||
+    //         password2.length == 0
+    //     ) {
+    //         setError(true)
+    //     }
+    // }
+
     return (
         <Flex>
             <Box>
@@ -88,12 +105,19 @@ const SignUpTab = () => {
                         </FormControl>
                         <FormControl>
                             <FormLabel>Password-Confirm</FormLabel>
-                            <Input type='password' placeholder='Re-enter your password' />
+                            <Input
+                                type='password'
+                                placeholder='Re-enter your password'
+                                value={password2}
+                                onChange={(e) => {
+                                    setPassword2(e.target.value)
+                                }}
+                            />
                         </FormControl>
                     </form>
                     <Center>
                         <Button size='lg' colorScheme='cyan' mt='24px' onClick={onClick}>
-                            <Link to='/'>Complete Sign Up</Link> to mainpage
+                            <Link to='/'>Complete Sign Up</Link>
                         </Button>
                     </Center>
                 </Box>
