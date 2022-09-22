369c648f057af32364be92f75c4a79d34fc59776
ktmorical <87923367+ktmorical@users.noreply.github.com>
2022-09-22 13:11:40 -0400

    Tabs and Leetcodeish Layout

--- frontend/src/pages/Programming.tsx
+++ frontend/src/pages/Programming.tsx
@@ -1,57 +1,42 @@
 import React, { useState } from 'react'
 import Editor from '@monaco-editor/react';
-import { Box, Button, Flex, HStack, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, VStack } from '@chakra-ui/react'
+import { Box, Button, Divider, HStack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 function Programming() {
-    const [code, setCode] = useState('');
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const initialValue = '// Enter Strategy Here'
+    const [code, setCode] = useState(initialValue);
     return (
-        <>
         <Box>
-        <HStack spacing='20%'>
-            <Box width='33%' margin='4'>
-                <VStack>
-                    <Editor
-                    height='75vh'
-                    defaultLanguage="javascript"
-                    defaultValue="// Enter Strategy Here"
-                    theme='vs-dark'
-                    onChange={(value, e) => setCode(value === undefined ? '' : value)}
-                    />
-                    <Box>
-                        <Button size='lg' colorScheme='cyan' mt='24px' onClick={() => {console.log(code)}}>
-                            Save Draft
-                        </Button>
-                    </Box>
-                </VStack>
-            </Box>
-            <Box marginTop='5'>
-                <VStack>
-                    <img src='/10x10 chess board.png' className='App-logo' alt='logo'/>
-                    <Box>
-                        <Button size='lg' colorScheme='cyan' mt='24px' variant='outline' onClick={onOpen}>
-                            View Game Rules
-                        </Button>
-                    </Box>
-                </VStack>
-            </Box>
-      </HStack>
+          <HStack>
+            <Box width='45%' borderRadius='1g' borderWidth='1px'>
+              <Tabs variant='enclosed'>
+                  <TabList>
+                    <Tab>
+                      Game Canvas
+                    </Tab>
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel height='82vh'>
+                      <img src='/10x10 chess board.png' className='App-logo' alt='logo'/>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </Box>
+              <Box width='55%' height='85vh'>
+                <Editor
+                defaultLanguage="javascript"
+                defaultValue={initialValue}
+                theme='vs-dark'
+                onChange={(value) => setCode(value === undefined ? '' : value)}
+                />
+              </Box>
+        </HStack>
+        <Divider orientation='horizontal'/>
+        <Box  mx='3' borderRadius='1g' borderWidth='1px' width='98%' display='flex' flexDirection='row' justifyContent='flex-end'>
+          <Button margin='3'>
+            Run Strategy
+          </Button>
+        </Box>
       </Box>
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Game Rules</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <p>Wow it opened</p>
-                </ModalBody>
-                <ModalFooter>
-                  <Button colorScheme='blue' mr={3} onClick={onClose}>
-                    Close
-                  </Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-        </>
       )
 }
 export default Programming
