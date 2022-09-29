ea5c6815375db29d8eb5c4431400b2f997463f90
ktmorical <87923367+ktmorical@users.noreply.github.com>
2022-09-28 21:18:30 -0400

    Fetch Game Data Structure and Populate Frontend

--- frontend/src/pages/Programming.tsx
+++ frontend/src/pages/Programming.tsx
@@ -1,102 +1,99 @@
 import React, { useState } from 'react'
 import Editor from '@monaco-editor/react';
-import { Box, Button, Code, Divider, Grid, GridItem, HStack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
-import { Game, Strategy } from '../../Models/Models'
+import { Box, Button, Center, Code, Divider, Grid, GridItem, Heading, HStack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { useParams } from 'react-router-dom';
+import useAVAFetch from '../helpers/useAVAFetch'
 
 function Programming() {
-    const initialValue = '// Enter Strategy Here'
-    const [code, setCode] = useState(initialValue);
-    const [buffer, setBuffer] = useState(0);
-    const [stockCode, setStockCode] = useState('')
-    
-    const updateSave = (value) => {
-      setCode(value === undefined ? '' : value)
-      if (buffer + 1 > 10) {
-        localStorage.setItem('draft', code);
-        localStorage.setItem('draftAvailable', 'true');
-        setBuffer(0);
-      } else {
-        setBuffer(buffer+1)
-      }
-  }
-  async function fetchStock() {
-    // TODO update URL
-    fetch('/getAI', {
-      method: 'POST',
-      body: ''
-    }).then(response => response.json()).then(stock =>
-      setStockCode(stock as string))
-  }
+  const initialValue = '// Enter Strategy Here'
+  const [code, setCode] = useState(initialValue);
+  const [buffer, setBuffer] = useState(0);
+  const { id } = useParams();
+  const { data } = useAVAFetch(id === undefined ? '/Games/1' : '/Games/');
+  console.log(data);
 
+    
+  const updateSave = (value) => {
+    setCode(value === undefined ? '' : value)
+    if (buffer + 1 > 10) {
+      localStorage.setItem('draft', code);
+      localStorage.setItem('draftAvailable', 'true');
+      setBuffer(0);
+    } else {
+      setBuffer(buffer+1)
+    }
+  }
     return (
-        <Box>
-          <HStack>
-            <Box width='45%' borderRadius='1g' borderWidth='1px'>
-              <Tabs variant='enclosed'>
-                  <TabList>
-                    <Tab>
-                      Game Canvas
-                  </Tab>
-                  <Tab>
-                    Game Description
-                  </Tab>
-                    <Tab>
-                      Stock Code
-                    </Tab>
-                    <Tab>
-                      Submission Statistics
-                    </Tab>
-                  </TabList>
-                  <TabPanels height='82vh'>
-                    <TabPanel>
-                      <img src='/10x10 chess board.png' className='App-logo' alt='logo'/>
-                    </TabPanel>
-                    <TabPanel>
-                  <p>{ 'No description found'}</p>
-                    </TabPanel>
-                <TabPanel onClick={fetchStock}>
+      <Box>
+        <HStack>
+          <Box width='45%' borderRadius='1g' borderWidth='1px'>
+            <Tabs variant='enclosed'>
+              <TabList>
+                <Tab>
+                  Game Canvas
+                </Tab>
+                <Tab>
+                  Game Description
+                </Tab>
+                <Tab>
+                  Stock Code
+                </Tab>
+                <Tab>
+                  Submission Statistics
+                </Tab>
+              </TabList>
+              <TabPanels height='82vh'>
+                <TabPanel>
+                  <img src='/10x10 chess board.png' className='App-logo' alt='logo'/>
+                </TabPanel>
+                <TabPanel>
+                  <Center>
+                    <Heading margin='3'>{data === undefined ? 'Game Name': data.name}</Heading>
+                  </Center>
+                  <p>{data === undefined ? 'Game Description': data.longDescription}</p>
+                </TabPanel>
+                <TabPanel>
                   <Code>
-                    {stockCode}
+                    {data === undefined ? 'Game Boilerplate': data.boilerplateCode}
                   </Code>
-                    </TabPanel>
-                  <TabPanel>
-                      </TabPanel>
-                  </TabPanels>
-                </Tabs>
-              </Box>
-              <Box width='55%' height='85vh'>
-                <Editor
-                defaultLanguage="javascript"
-                defaultValue={localStorage.getItem('draftAvailable') === 'true' ? localStorage.getItem('draft') || initialValue : initialValue}
-                theme='vs-dark'
-                onChange={(value) => updateSave(value)}
-                />
-              </Box>
-        </HStack>
-        <Divider orientation='horizontal'/>
-        <Grid>
-          <GridItem>
-            <Button variant='outline' margin='3' color={'green'}>
-              Easy Stock
-            </Button>
-            <Button variant='outline' margin='3' color={'orange'} disabled>
-              Medium Stock
-            </Button>
-            <Button variant='outline' margin='3' color={'red'} disabled>
-              Hard Stock
-            </Button>
-          </GridItem>
-          <GridItem colStart={9}>
-            <Button margin='3'>
-              Run Strategy
-            </Button>
-            <Button margin='3' disabled>
-              Submit Strategy
-            </Button>
-          </GridItem>
-        </Grid>
-          
-      </Box>
+                </TabPanel>
+                <TabPanel>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+          <Box width='55%' height='85vh'>
+            <Editor
+            defaultLanguage="javascript"
+            defaultValue={localStorage.getItem('draftAvailable') === 'true' ? localStorage.getItem('draft') || initialValue : initialValue}
+            theme='vs-dark'
+            onChange={(value) => updateSave(value)}
+            />
+          </Box>
+      </HStack>
+      <Divider orientation='horizontal'/>
+      <Grid>
+        <GridItem>
+          <Button variant='outline' margin='3' color={'green'}>
+            Easy Stock
+          </Button>
+          <Button variant='outline' margin='3' color={'orange'} disabled>
+            Medium Stock
+          </Button>
+          <Button variant='outline' margin='3' color={'red'} disabled>
+            Hard Stock
+          </Button>
+        </GridItem>
+        <GridItem colStart={9}>
+          <Button margin='3'>
+            Run Strategy
+          </Button>
+          <Button margin='3' disabled>
+            Submit Strategy
+          </Button>
+        </GridItem>
+      </Grid> 
+    </Box>
       )
 }
 export default Programming
