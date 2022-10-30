ee6ff369b822eaef2a49893625e9a85e9b44c589
QihaoGe <ge69@purdue.edu>
2022-10-30 11:18:07 -0400

    some route connection

--- frontend/src/pages/profile/ProfileBattlesTab.tsx
+++ frontend/src/pages/profile/ProfileBattlesTab.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AVAStore } from '../../data/DataStore'
+import IdentityService from '../../data/IdentityService'
+import useAVAFetch from '../../helpers/useAVAFetch'
+
 import {
     Center,
     Box,
@@ -13,21 +18,23 @@ import {
 } from '@chakra-ui/react'
 
 const ProfileBattlesTab = () => {
+    const navigate = useNavigate()
+
     return (
         <Stack>
             <Box maxW='32rem'>
                 <Menu>
                     <MenuButton as={Button}>Open Most Recent Battles</MenuButton>
                     <MenuList>
-                        <MenuItem>Battle 1</MenuItem>
+                        <MenuItem onClick={() => navigate('/Profile')}>Battle 1</MenuItem>
                         <MenuDivider />
-                        <MenuItem>Battle 2</MenuItem>
+                        <MenuItem onClick={() => navigate('/Profile')}>Battle 2</MenuItem>
                         <MenuDivider />
-                        <MenuItem>Battle 3</MenuItem>
+                        <MenuItem onClick={() => navigate('/Profile')}>Battle 3</MenuItem>
                         <MenuDivider />
-                        <MenuItem>Battle 4</MenuItem>
+                        <MenuItem onClick={() => navigate('/Profile')}>Battle 4</MenuItem>
                         <MenuDivider />
-                        <MenuItem>Battle 5</MenuItem>
+                        <MenuItem onClick={() => navigate('/Profile')}>Battle 5</MenuItem>
                     </MenuList>
                 </Menu>
             </Box>
