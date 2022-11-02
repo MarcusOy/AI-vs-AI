6209d4f4e15eba3cd45114a7cf8dff5c353f3661
david1 <BdavidwuS@gmail.com>
2022-11-02 18:16:53 -0400

    Update MediumAI.java
    done with MediumAI except for the restoring formation part

--- simulation/src/main/java/IStrategy/MediumAI.java
+++ simulation/src/main/java/IStrategy/MediumAI.java
@@ -5,6 +5,21 @@ import Simulation.GameState;
 
 import java.util.ArrayList;
 
+/**
+ * Capture moves will now be returned in an 9-array of ArrayLists with the following priorities
+ * F2 captures 1
+ * 3 captures 1
+ * B2 captures 1
+ * 4 captures 1
+ * 3 captures B2
+ * 4 captures B2
+ * 3 captures F2
+ * 4 captures F2
+ * 4 captures 3
+ *
+ * The tiebreaker for all moves is whatever leaves the least non-formation pieces (1s and F2s) exposed to capture,
+ * 1s being valued more (1s valued at 11, F2s valued at 10)
+ */
 public class MediumAI implements IStrategy {
     /**
      * API containing helper functions
@@ -97,7 +112,42 @@ public class MediumAI implements IStrategy {
                     }
                 }
             default:            //finished setting up formation phase
-                return getRandomMove(gameState);
+                if (api.isPlayerInCheck(api.getMyColor(gameState), board) == api.TRUE) {
+                    ArrayList<String>[] movesArray = getAllLegalCaptureMovesDifferentiated(gameState);
+                    for (int i = 0; i < 9; i++) {
+                        if (movesArray[i].isEmpty())
+                            continue;
+                        return pickBestMoveFromList(gameState, movesArray[i]);
+                    }
+
+                    ArrayList<String> moves = getAllLegalTrade1For1Moves(gameState);
+                    if (!moves.isEmpty()) {
+                        return pickBestMoveFromList(gameState, moves);
+                    } else {    //no legal moves, means checkmated
+                        return "CHECKMATED";
+                    }
+                } else {
+                    ArrayList<String>[] movesArray = getAllLegalCaptureMovesDifferentiated(gameState);
+                    for (int i = 0; i < 9; i++) {
+                        if (movesArray[i].isEmpty())
+                            continue;
+                        return pickBestMoveFromList(gameState, movesArray[i]);
+                    }
+                    ArrayList<String> moves = getAllLegalTradeF2ForB2Moves(gameState);
+                    if (!moves.isEmpty()) {
+                        return pickBestMoveFromList(gameState, moves);
+                    }
+                    moves = getAllLegalTrade1For1Moves(gameState);
+                    if (!moves.isEmpty()) {
+                        return pickBestMoveFromList(gameState, moves);
+                    }
+                    moves = getAllLegalNonCaptureNonFormationMoves(gameState);
+                    if (!moves.isEmpty()) {
+                        return pickBestMoveFromList(gameState, moves);
+                    }
+                }
+                //it technically should never get here
+                throw new AssertionError("either we got checkmated or error");
         }
     }
 
@@ -139,6 +189,133 @@ public class MediumAI implements IStrategy {
                     break;
 
                 moves.add(piece + ", " + move);
+            }
+        }
+        return moves;
+    }
+
+    public ArrayList<String>[] getAllLegalCaptureMovesDifferentiated(GameState gameState) {
+        String board[][] = api.getBoard(gameState);
+        String[] pieceLocations = api.getMyPieceLocations(api.getMyColor(gameState), board);
+
+        ArrayList<String>[] movesArray = (ArrayList<String>[])(new Object[9]);
+        for (int i = 0; i < 9; i++) {
+            movesArray[i] = new ArrayList<String>();
+        }
+
+        for (String piece : pieceLocations) {
+            if (piece.equals(""))
+                break;
+
+            String[] validMoves = api.getValidMoves(piece, api.getMyColor(gameState), board);
+            for (String move : validMoves) {
+                if (move.equals(""))
+                    break;
+
+                //a valid move is guaranteed to land on either an empty space or one with an opposing piece
+                if (api.getPieceMoveDistance(move, board) != 0 &&
+                        api.getPieceMoveDistance(piece, board) > api.getPieceMoveDistance(move, board)) {
+                    if (api.getPieceMoveDistance(move, board) == 1) {
+                        if (api.getPieceMoveDistance(piece, board) == 2) {
+                            if (api.cellToRow(piece) % 2 != api.getMyColor(gameState)) {        //back rank 2
+                                movesArray[2].add(piece + ", " + move);
+                            } else {            //front rank 2
+                                movesArray[0].add(piece + ", " + move);
+                            }
+                        } else if (api.getPieceMoveDistance(piece, board) == 3) {
+                            movesArray[1].add(piece + ", " + move);
+                        } else {           //4
+                            movesArray[3].add(piece + ", " + move);
+                        }
+                    } else if (api.getPieceMoveDistance(move, board) == 2) {
+                        if (api.cellToRow(move) % 2 != api.getOpponentColor(api.getMyColor(gameState))) {        //back rank 2
+                            if (api.getPieceMoveDistance(piece, board) == 3) {
+                                movesArray[4].add(piece + ", " + move);
+                            } else {        //4
+                                movesArray[5].add(piece + ", " + move);
+                            }
+                        } else {            //front rank 2
+                            if (api.getPieceMoveDistance(piece, board) == 3) {
+                                movesArray[6].add(piece + ", " + move);
+                            } else {        //4
+                                movesArray[7].add(piece + ", " + move);
+                            }
+                        }
+                    } else {        //4 captures 3
+                        movesArray[8].add(piece + ", " + move);
+                    }
+                }
+            }
+        }
+
+        return movesArray;
+    }
+
+    public ArrayList<String> getAllLegalNonCaptureNonFormationMoves(GameState gameState) {
+        String board[][] = api.getBoard(gameState);
+        String[] pieceLocations = api.getMyPieceLocations(api.getMyColor(gameState), board);
+
+        ArrayList<String> moves = new ArrayList<String>();
+        for (String piece : pieceLocations) {
+            if (piece.equals(""))
+                break;
+
+            String[] validMoves = api.getValidMoves(piece, api.getMyColor(gameState), board);
+            for (String move : validMoves) {
+                if (move.equals(""))
+                    break;
+
+                if (api.getPieceMoveDistance(move, board) == 0) {
+                    if (api.getPieceMoveDistance(piece, board) == 1 || //front rank 2
+                            (api.getPieceMoveDistance(piece, board) == 2) && api.cellToRow(piece) % 2 == api.getMyColor(gameState)) {
+                        moves.add(piece + ", " + move);
+                    }
+                }
+            }
+        }
+        return moves;
+    }
+
+    public ArrayList<String> getAllLegalTradeF2ForB2Moves(GameState gameState) {
+        String board[][] = api.getBoard(gameState);
+        String[] pieceLocations = api.getMyPieceLocations(api.getMyColor(gameState), board);
+
+        ArrayList<String> moves = new ArrayList<String>();
+        for (String piece : pieceLocations) {
+            if (piece.equals(""))
+                break;
+
+            String[] validMoves = api.getValidMoves(piece, api.getMyColor(gameState), board);
+            for (String move : validMoves) {
+                if (move.equals(""))
+                    break;
+
+                if (api.getPieceMoveDistance(piece, board) == 2 && api.getPieceMoveDistance(move, board) == 2
+                        && api.cellToRow(piece) % 2 == api.getMyColor(gameState)) { //front rank 2
+                    moves.add(piece + ", " + move);
+                }
+            }
+        }
+        return moves;
+    }
+
+    public ArrayList<String> getAllLegalTrade1For1Moves(GameState gameState) {
+        String board[][] = api.getBoard(gameState);
+        String[] pieceLocations = api.getMyPieceLocations(api.getMyColor(gameState), board);
+
+        ArrayList<String> moves = new ArrayList<String>();
+        for (String piece : pieceLocations) {
+            if (piece.equals(""))
+                break;
+
+            String[] validMoves = api.getValidMoves(piece, api.getMyColor(gameState), board);
+            for (String move : validMoves) {
+                if (move.equals(""))
+                    break;
+
+                if (api.getPieceMoveDistance(piece, board) == 1 && api.getPieceMoveDistance(move, board) == 1) {
+                    moves.add(piece + ", " + move);
+                }
             }
         }
         return moves;
@@ -202,6 +379,104 @@ public class MediumAI implements IStrategy {
             }
         }
         return moves;
+    }
+
+    public String[][] tryThisMove(GameState gameState, String moveString) {
+        String[][] board = api.getBoard(gameState);
+        String[] moveCells = moveString.split(", ");
+
+        String fromCell = moveCells[0];
+        String toCell = moveCells[1];
+
+        int fromMoveDistance = api.getPieceMoveDistance(fromCell, board);
+        int toMoveDistance = api.getPieceMoveDistance(toCell, board);
+        int fromCol = api.cellToCol(fromCell);
+        int fromRow = api.cellToRow(fromCell);
+        int toCol = api.cellToCol(toCell);
+        int toRow = api.cellToRow(toCell);
+        String fromPiece = board[fromCol][fromRow];
+        String toPiece = board[toCol][toRow];
+
+        // copies piece to new location
+        if (fromMoveDistance > toMoveDistance) { // attacker wins
+            board[toCol][toRow] = board[fromCol][fromRow];
+        } else if (fromMoveDistance == toMoveDistance) { // both pieces die upon a tie
+            board[toCol][toRow] = "";
+        } else { // if (fromMoveDistance < toMoveDistance) // defender wins
+            return null;    //return null to show that this is a suicide move which is very bad and shouldn't be considered
+        }
+        // removes piece from old location
+        board[fromCol][fromRow] = "";
+        return board;
+    }
+
+    public int nonFormationPiecesExposedToCapture(GameState gameState, String[][] newBoard) {
+        if (newBoard == null) {     //the move tried was a suicide move, avoid at all costs
+            return 1000000;
+        }
+
+        String[] myPieceLocations = api.getMyPieceLocations(api.getMyColor(gameState), newBoard);
+        ArrayList<String> my1PieceLocations = new ArrayList<String>();
+        ArrayList<String> myF2PieceLocations = new ArrayList<String>();
+        for (String piece : myPieceLocations) {
+            if (piece.equals(""))
+                break;
+            if (api.getPieceMoveDistance(piece, newBoard) == 1) {
+                my1PieceLocations.add(piece);
+            } else if (api.getPieceMoveDistance(piece, newBoard) == 2 && api.cellToRow(piece) % 2 == api.getMyColor(gameState)) {   //front rank 2
+                myF2PieceLocations.add(piece);
+            }
+        }
+
+        int my1PiecesRemaining = my1PieceLocations.size();
+        int myF2PiecesRemaining = myF2PieceLocations.size();
+        //now we check how many capturing moves the OPPONENT has
+        String[] opponentPieceLocations = api.getMyPieceLocations(api.getOpponentColor(api.getMyColor(gameState)), newBoard);
+        for (String piece : opponentPieceLocations) {
+            if (piece.equals(""))
+                break;
+
+            String[] validMoves = api.getValidMoves(piece, api.getOpponentColor(api.getMyColor(gameState)), newBoard);
+            for (String move : validMoves) {
+                if (move.equals(""))
+                    break;
+
+                //a valid move is guaranteed to land on either an empty space or one with an opposing piece
+                if (api.getPieceMoveDistance(move, newBoard) != 0 &&
+                        api.getPieceMoveDistance(piece, newBoard) > api.getPieceMoveDistance(move, newBoard)) {
+                    //start crossing off locations of 1s and F2s exposed to capture
+                    if (my1PieceLocations.contains(move)) {
+                        my1PieceLocations.remove(move);
+                    } else if (myF2PieceLocations.contains(move)) {
+                        myF2PieceLocations.remove(move);
+                    }
+                }
+            }
+        }
+        return (my1PiecesRemaining - my1PieceLocations.size()) * 11 + (myF2PiecesRemaining - myF2PieceLocations.size()) * 10;
+    }
+
+    public String pickBestMoveFromList(GameState gameState, ArrayList<String> moves) {
+        //moves must not be empty
+        if (moves.isEmpty()) {
+            throw new AssertionError("i think we got checkmated");
+        }
+
+        int bestMoveIndex = 0;
+        int bestMoveEvaluation = 1000001;
+        for (int j = 0; j < moves.size(); j++) {
+            int evaluation = nonFormationPiecesExposedToCapture(gameState, tryThisMove(gameState, moves.get(j)));
+            if (evaluation < bestMoveEvaluation) {
+                bestMoveIndex = j;
+                bestMoveEvaluation = evaluation;
+            } else if (evaluation == bestMoveEvaluation) {      //switch between equal moves with probability 0.1
+                if (Math.random() < 0.1) {
+                    bestMoveIndex = j;
+                    bestMoveEvaluation = evaluation;
+                }
+            }
+        }
+        return moves.get(bestMoveIndex);
     }
 
     public boolean isInPartialFormation(GameState gameState) {
