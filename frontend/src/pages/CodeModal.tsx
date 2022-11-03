f47849a0efda9dbed058c88ddcda582eba85e723
ktmorical <87923367+ktmorical@users.noreply.github.com>
2022-11-02 20:10:21 -0400

    Add Duplicate and Copy to Stock Code

--- frontend/src/pages/CodeModal.tsx
+++ frontend/src/pages/CodeModal.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Code, IconButton, Modal, Center, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
+import ModalAi from './ModalAi';
+import Copy from '../helpers/Copy';
 interface CodeModalProps {
     codeName?: string
     code: string
@@ -27,10 +29,14 @@ const CodeModal = (props: CodeModalProps) => {
                         </Center>
                     </ModalBody>
             
-                    <ModalFooter>
-                        <Center>
+                    <ModalFooter p='2'>
+                        <ModalAi overwrite={true} strategy={{sourceCode: props.code, name: props.codeName || 'Duplicated Strategy'}} />
+                        <Center ml='4'>
+                            <Copy sourceCode={props.code} />
+                            </Center>
+                        <Center ml='4'>
                             <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
+                                Close
                             </Button>
                         </Center>
                     </ModalFooter>
