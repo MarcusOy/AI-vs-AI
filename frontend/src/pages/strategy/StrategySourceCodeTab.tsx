ea1c53d33204e8ae73206ff0944ca2471be479d3
QihaoGe <ge69@purdue.edu>
2022-11-03 17:26:56 -0400

    fixing

--- frontend/src/pages/strategy/StrategySourceCodeTab.tsx
+++ frontend/src/pages/strategy/StrategySourceCodeTab.tsx
@@ -1,25 +1,50 @@
-import React from 'react'
-import { Strategy } from '../../models/strategy'
+import React, { useEffect, useRef, useState } from 'react'
 import {
-    Center,
-    Box,
-    Text,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
     Button,
-    Stack,
-    MenuDivider,
-    Avatar,
+    Code,
+    IconButton,
+    Modal,
+    Center,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+    Box,
 } from '@chakra-ui/react'
+import { AddIcon, ViewIcon } from '@chakra-ui/icons'
+import CodeView from '../CodeView'
+import useAVAFetch from '../../helpers/useAVAFetch'
+import { Link, useParams } from 'react-router-dom'
+import { Strategy } from '../../models/strategy'
 
-interface IStrategySourceCodePage {
+import {
+    devComplete,
+    developerAi,
+    easyAi,
+    emptyStarter,
+    helperFunctions,
+} from '../../helpers/hardcodeAi'
+interface StrategySourceCodeTab {
     strategy: Strategy
 }
 
-const StrategySourceCodeTab = (p: IStrategySourceCodePage) => {
-    return <Text>hi</Text>
+const StrategySourceCodeTab = (p: StrategySourceCodeTab) => {
+    const [select, setSelect] = useState(false)
+    const { id } = useParams()
+    const sourceCode = p.strategy.sourceCode
+    const strategy = useAVAFetch('/getAi/' + id).data
+    return (
+        <Box>
+            <Center>
+                <Code colorScheme={'gray'}>
+                    <pre>{sourceCode}</pre>
+                </Code>
+            </Center>
+        </Box>
+    )
 }
 
 export default StrategySourceCodeTab
