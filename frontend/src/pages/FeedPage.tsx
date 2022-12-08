ad9bf539aac895b5504ad9125f59a08ae07ed80a
ktmorical <87923367+ktmorical@users.noreply.github.com>
2022-12-08 01:58:29 -0500

    Reformat Feed, Daily Stats & Relative Time,  Removed SysUser from feed

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { Center, Box, Text, Flex, Spacer, Heading, Button, VStack, HStack, Avatar, Badge, Stack, Input, Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Stat, StatLabel, StatNumber, StatHelpText, Box, Switch, Text, Flex, Spacer, Heading, Button, VStack, HStack, Avatar, Badge, Stack, IconButton, Popover, PopoverBody, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
 import ModalAi from './ModalAi'
 import { Link, useNavigate } from 'react-router-dom'
 import useAVAFetch from '../helpers/useAVAFetch'
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { ChevronRightIcon, HamburgerIcon } from '@chakra-ui/icons'
 import { TbBook2 } from 'react-icons/tb'
-import { StrategyStatus } from '../models/strategy-status'
 import { randomColor } from '@chakra-ui/theme-tools'
 import { ResultType } from '../models/result-type'
-interface Interactions {
-    type: number
-    title: string
-    sourceCode?: string
-    id: string
-    createdByGuid: string
-    createdByName: string
-    time: string
-}
+import moment from 'moment'
+import { InteractionType } from '../models/interaction-type'
 const FeedPage = () => {
     const navigate = useNavigate()
     const [showFilters, setShowFilters] = useState([false, false, false]);
@@ -24,55 +16,82 @@ const FeedPage = () => {
         '/Interactions/',
         { method: 'GET' },
     )
+    const daily = useAVAFetch(
+        '/Interactions/DailyStats',
+        { method: 'GET' },
+    ).data
     const selectNavigation = async (r) => {
         if (r.type == ResultType.User) navigate(`/Profile/${r.id}/View`)
         else if (r.type == ResultType.Strategy || r.type == ResultType.Battle) navigate(`/Strategy/${r.id}/Stats`)
         else navigate('/Invalid')
     }
+    console.log(data)
     return (
         <Box>
             <Box>
-            <HStack spacing={'39vw'} width='100%'>
-            <Box>
-                <ModalAi overwrite={false} />
-            </Box>
-            <Box>
-                <Text>Feed page</Text>
-                <Link to='/ManualPlay'> Manual Play </Link>
-            </Box>
-                </HStack>
-            </Box>
-            <Box>
                 <VStack my='2'>
-                    <Box border={'1px'} borderColor='light-gray' w='50vw' display={'flex'} alignContent='center'>
-                     <Accordion allowToggle w={'50vw'}>
-                                <AccordionItem >
-                                    <h2>
-                                        <AccordionButton>
-                                            <Box flex='1' textAlign='left'>
-                                                Filters
-                                            </Box>
-                                            <AccordionIcon />
-                                        </AccordionButton>
-                                    </h2>
-                                    <AccordionPanel pb={4}>
-                                        <Box  marginY='4' justifyContent='center'>
-                                        <Button mx='2' onClick={() => setShowFilters([!showFilters[0], showFilters[1], showFilters[2]])}>
-                                            {!showFilters[0] ? 'Hide Created Users' : 'Show Created Users'}</Button>
-                                        <Button mx='2' onClick={() => setShowFilters([showFilters[0], !showFilters[1], showFilters[2]])}>
-                                            {!showFilters[1] ? 'Hide Created Drafts' : 'Show Created Drafts'}</Button>
-                                        <Button onClick={() => setShowFilters([showFilters[0], showFilters[1], !showFilters[2]])}>
-                                            {!showFilters[2] ? 'Hide Activated Strategies' : 'Show Activated Strategies'}</Button>
-                                        </Box>
-                                    </AccordionPanel>
-                                </AccordionItem>
-                        </Accordion>
+                    <HStack w='50vw'>
+                        <ModalAi overwrite={false} />
+                        <Spacer/>
+                        <Heading as='h2' size='xl'>
+                            Feed
+                        </Heading> 
+                        <Popover placement='right' matchWidth={true} size='sm' offset={[25, 6]}>
+                        <PopoverTrigger>
+                        <IconButton icon={<HamburgerIcon />} aria-label='filters' />
+                        </PopoverTrigger>
+                        <PopoverContent>
+                                <PopoverBody>
+                                    <VStack>
+                                        <Box display={'inherit'} gap='2'>
+                                        <Text>Created Users</Text>
+                                            <Switch defaultChecked onChange={() => setShowFilters([!showFilters[InteractionType.User],
+                                            showFilters[InteractionType.CreatedStrategy], showFilters[InteractionType.SubmittedStrategy]])} />
 
-                    </Box>
+                                        </Box>
+                                        <Box display={'inherit'} gap='2'>
+                                        <Text>Created Drafts</Text>
+                                            <Switch defaultChecked onChange={() => setShowFilters([showFilters[InteractionType.User],
+                                            !showFilters[InteractionType.CreatedStrategy], showFilters[InteractionType.SubmittedStrategy]])} />
+
+                                        </Box>
+                                        <Box display={'inherit'} gap='2'>
+                                        <Text>Activated Strategies</Text>
+                                            <Switch defaultChecked onChange={() => setShowFilters([showFilters[InteractionType.User],
+                                            showFilters[InteractionType.CreatedStrategy], !showFilters[InteractionType.SubmittedStrategy]])} />
+                                        </Box>
+                                        </VStack>
+                            </PopoverBody>
+                        </PopoverContent>
+                        </Popover>
+                        <Spacer/>
+                        <Box mt='1'>
+                            <Link to='/ManualPlay'> Manual Play </Link> 
+                            </Box>
+                    </HStack>
+                    <Flex w={'50vw'}>
+                           <Stat>
+                            <StatLabel>New Users</StatLabel>
+                            <StatNumber>{daily == undefined ? 0 : daily[InteractionType.User]}</StatNumber>
+                            <StatHelpText>Since {moment().add(-1, 'day').format('h:mm a, MM/DD')} </StatHelpText>
+                            </Stat>
+                        <Spacer />   
+                        <Stat>
+                            <StatLabel>New Drafts</StatLabel>
+                            <StatNumber>{daily == undefined ? 0 : daily[InteractionType.CreatedStrategy]}</StatNumber>
+                            <StatHelpText>Since {moment().add(-1, 'day').format('h:mm a, MM/DD')} </StatHelpText>
+                            </Stat> 
+                        <Spacer />
+                        <Stat>
+                            <StatLabel>New Activations</StatLabel>
+                            <StatNumber>{daily == undefined ? 0 : daily[InteractionType.SubmittedStrategy]}</StatNumber>
+                            <StatHelpText>Since {moment().add(-1, 'day').format('h:mm a, MM/DD')} </StatHelpText>
+                            </Stat>
+                            </Flex>
                 {data?.filter(function (item) {
-                    if (!(showFilters[0] && item.type == 0)
-                        && !(showFilters[1] && item.type == 1)
-                        && !(showFilters[2] && item.type == 2))
+                    if (!(showFilters[InteractionType.User] && item.type == InteractionType.User)
+                        && !(showFilters[InteractionType.CreatedStrategy] && item.type == InteractionType.CreatedStrategy)
+                        && !(showFilters[InteractionType.SubmittedStrategy] && item.type == InteractionType.SubmittedStrategy))
                         return item
                 }).map((item, index) => {
                     return (
@@ -85,32 +104,34 @@ const FeedPage = () => {
                             w={'50vw'}
                             h={'10vh'}
                         >
-                                <Avatar bg={randomColor({ string: item.title })} icon={(item.type !== 0 && <TbBook2 size='25' />) || undefined}
-                                    name={item.type == 0 && item.title || undefined} />
+                                <Avatar bg={randomColor({ string: item.title })} icon={(item.type !== InteractionType.User && <TbBook2 size='25' />) || undefined}
+                                    name={item.type == InteractionType.User && item.title || undefined} />
                             <Stack spacing='0.2rem' textAlign='left' ml={5}>
                                 <Text>{item.title}</Text>
                                 <Text fontSize='xs'>
                                     <span style={{ marginRight: 10 }}> </span>
-                                    {item.type == 1 && (
-                                        <Badge variant='outline' colorScheme='cyan'>
+                                    {item.type == InteractionType.CreatedStrategy && (
+                                        <Badge mr='2' variant='outline' colorScheme='cyan'>
                                             Draft Strategy
                                         </Badge>
                                     )}
-                                    {item.type == 2 && (
-                                        <Badge variant='solid' colorScheme='cyan'>
+                                    {item.type == InteractionType.SubmittedStrategy && (
+                                        <Badge mr='2' variant='solid' colorScheme='cyan'>
                                             Active Strategy
                                         </Badge>
                                     )}
-                                    {item.type == 0 && (
-                                        <Badge variant='solid' colorScheme='cyan'>
+                                    {item.type == InteractionType.User && (
+                                        <Badge mr='2' variant='solid' colorScheme='cyan'>
                                             Created User
                                         </Badge>
-                                    )}
-                                </Text>
+                                        )}
+                                       {moment(item.time, 'YYYY-MM-DDThh:mm:ss').add(-5, 'hour').fromNow()}
+                                    </Text>
+                                    
                             </Stack>
                             <Box flexGrow={1} />
                             </Button>
-                            </Box>
+                        </Box>
                     )
                 })}
                     </VStack>
