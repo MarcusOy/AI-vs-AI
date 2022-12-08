60f2e1177f6c595679ff4a92cad36f4d8b397100
wzollma <wzollma@purdue.edu>
2022-12-07 23:12:24 -0500

    Finished decompressExecutionTrace algorithm

--- frontend/src/helpers/decompressExecTrace.ts
+++ frontend/src/helpers/decompressExecTrace.ts
@@ -1,4 +1,4 @@
-/*interface IStack<T> {
+interface IStack<T> {
     push(item: T): void;
     pop(): T | undefined;
     peek(): T | undefined;
@@ -30,22 +30,6 @@ class Stack<T> implements IStack<T> {
     }
 }
 
-const compString: string = "2-10|12|<<13-15|17-19>x3|13-16|8-10|12|<13-15|17-19>x3|13-15|17-19>x2|<13-15|17-19>x2|13-16|8-10|12-15|17-19|13|14|<15|<17-19|13-15>x4|16|8-10|12-14>x2|15|17-19|13-15|17|<18|<19|13-15|17|18>x4|19|13-16|8-10|12|<13-15|17-19>x2|13-15|17>x2|18|19|13-15|17-19|<13|<14|15|17-19|13>x4|14-16|8-10|12-15|17-19|13-15|17-19>x2|13-16|8-10|12|<13-15|17-19>x3|13-16|8-10|12|<13-15|17-19>x5|13-16|8-11|23|26";
-const expected: string = "2|3|4|5|6|7|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|12|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|17|18|19|13|14|15|16|8|9|10|11|23|26";
-console.log(compString);
-const decompString: string = decompressExecTrace(compString);
-console.log(expected);
-console.log(decompString);
-let areEqual: boolean = decompString.length == expected.length;
-console.log("areEqual: " + areEqual);
-for (let i: number = 0; i < expected.length; i++) {
-    if (decompString[i] !== expected[i]) {
-        areEqual = false;
-        break;
-    }
-}
-console.log("areEqual: " + areEqual);
-
 function decompressExecTrace(compressedExecTrace: string): string {
     const result: string = "";
     const pipeSplit: string[] = compressedExecTrace.split("|");
@@ -57,8 +41,6 @@ function decompressExecTrace(compressedExecTrace: string): string {
     while (true) {
         const nextCycleStartIndex: number = workingString.indexOf("<");
         const nextCycleEndIndex: number = workingString.indexOf(">");
-        console.log("nextStartIndex: " + nextCycleStartIndex);
-        console.log("nextEndIndex: " + nextCycleEndIndex);
 
         let indexToUse: number;
         let startingCycle: boolean;
@@ -67,20 +49,18 @@ function decompressExecTrace(compressedExecTrace: string): string {
         if (nextCycleStartIndex >= 0 && (nextCycleStartIndex < nextCycleEndIndex || nextCycleEndIndex < 0)) {
             indexToUse = nextCycleStartIndex;
             startingCycle = true;
-            console.log("starting cycle");
         }
         // '>' is next closes
         else if (nextCycleEndIndex >= 0) {
             indexToUse = nextCycleEndIndex;
             startingCycle = false;
-            console.log("ending cycle");
         }
         // no more cycle parsing
         else {
             let popValue = cycleStack.pop();
             let popOptional: string = popValue == undefined ? "" : popValue;
-            cycleStack.push(uncompressSequence(popOptional + "|" + workingString));
-            console.log("done");
+            const strToPush: string = uncompressSequence(popOptional + workingString);
+            cycleStack.push(strToPush);
             break;
         }
 
@@ -91,9 +71,10 @@ function decompressExecTrace(compressedExecTrace: string): string {
             : workingString.substring(0, indexToUse);
         if (!hasStartedCycle && startingCycle)
             hasStartedCycle = true;
-        cycleStack.push(popOptional + stringToInsert);
+
+        const strToPush: string = popOptional + stringToInsert;
+        cycleStack.push(strToPush);
         workingString = workingString.substring(indexToUse + (startingCycle ? 1 : 2));
-        console.log("workingString: " + workingString);
 
         // either pushes or pops a new one depending on if starting or ending cycle
         if (startingCycle)
@@ -104,14 +85,12 @@ function decompressExecTrace(compressedExecTrace: string): string {
             while (workingString.length > 0) {
                 const curChar: string = workingString.charAt(0);
                 const curCharAsInt: number = parseInt(curChar);
-                console.log("curCharAsInt: " + curCharAsInt);
 
                 if (Number.isNaN(curCharAsInt))
                     break;
 
                 cycleLengthString += curChar;
                 workingString = workingString.substring(1);
-                console.log("workingString: " + workingString);
             }
             let cycleLength = parseInt(cycleLengthString);
 
@@ -127,17 +106,17 @@ function decompressExecTrace(compressedExecTrace: string): string {
 
             let popValue = cycleStack.pop();
             let popOptional: string = popValue == undefined ? "" : popValue;
-            cycleStack.push(popOptional + unrolledCycleString);
+            const strToPush: string = popOptional + unrolledCycleString;
+            cycleStack.push(strToPush);
         }
     }
 
     let popVal = cycleStack.pop();
 
-    return popVal == undefined ? "" : popVal;//result;
+    return popVal == undefined ? "" : popVal;
 }
 
 function uncompressSequence(input: String): string {
-    console.log("input: " + input);
     let uncompressedCycleString: string = "";
     const pipeSplit: string[] = input.split("|");
     for (let i: number = 0; i < pipeSplit.length; i++) {
@@ -148,7 +127,6 @@ function uncompressSequence(input: String): string {
         if (indexOfDash > 0) {
             const sequenceStartNum = parseInt(curSequence.substring(0, indexOfDash));
             const sequenceEndNum = parseInt(curSequence.substring(indexOfDash + 1));
-            console.log("start: " + sequenceStartNum + "   end: " + sequenceEndNum);
             modifedSequence += sequenceStartNum + "";
             for (let line: number = sequenceStartNum + 1; line <= sequenceEndNum; line++)
                 modifedSequence += "|" + line;
@@ -162,6 +140,5 @@ function uncompressSequence(input: String): string {
         uncompressedCycleString += modifedSequence;
     }
 
-    console.log("output: " + uncompressedCycleString);
     return uncompressedCycleString;
-}*/
+}
