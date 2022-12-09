c76e8ad3c81f8a13de303201f3b348f981ce844a
david1 <BdavidwuS@gmail.com>
2022-12-09 12:35:56 -0500

    user easy AI1

---
+++ backend/Data/init/games/2/strategies/User Easy AI1/code.js
@@ -0,0 +1,133 @@
+function getMove() {
+    var board = getBoard();
+    var pieceLocations = getMyPieceLocations(getMyColor());
+
+    switch (gameState.numMovesMade / 2 + 1) {
+        case 1:
+            if (getMyColor() === 0) {    //we are playing white
+                if (board[1][3] === ("b3") || board[4][3] === ("b3")) {
+                    return "F9, F5";            //move the RIGHT 4 if opponent moved the LEFT 3
+                } else if (board[5][3] === "b3" || board[9][3] === "b3") {
+                    return "E9, E5";            //move the LEFT 4 if opponent moved the RIGHT 3
+                } else {
+                    moves = new Array("E9, E5", "F9, F5");      //pick randomly
+                    return moves[Math.floor((Math.random() * moves.length))];
+                }
+            } else {                            //we are playing black
+                if (board[1][6] === ("w3") || board[4][6] === ("w3")) {
+                    return "F0, F4";            //move the RIGHT 4 if opponent moved the LEFT 3
+                } else if (board[5][6] === ("w3") || board[9][6] === ("w3")) {
+                    return "E0, E4";            //move the LEFT 4 if opponent moved the RIGHT 3
+                } else {
+                    moves = new Array("E0, E4", "F0, F4");      //pick randomly
+                    return moves[Math.floor((Math.random() * moves.length))];
+                }
+            }
+        case 2:
+            if (getMyColor() === 0) {    //we are playing white
+                if (board[5][5] === "w4") {      //we moved the RIGHT 4 for our first move
+                    if (board[5][3] === "b3" || board[9][3] === "b3") {
+                        return "E9, E5";            //move the LEFT 4 if opponent moved the RIGHT 3
+                    } else {                    //otherwise move the RIGHT 2
+                        return "H9, F9";
+                    }
+                } else if (board[4][5] === "w4") {                     //we moved the LEFT 4 for our first move
+                    if (board[1][3] === "b3" || board[4][3] === "b3") {
+                        return "F9, F5";            //move the RIGHT 4 if opponent moved the LEFT 3
+                    } else {                    //otherwise move the LEFT 2
+                        return "C9, E9";
+                    }
+                }
+            } else {                            //we are playing black
+                if (board[5][4] === "b4") {      //we moved the RIGHT 4 for our first move
+                    if (board[5][6] === "w3" || board[9][6] === "w3") {
+                        return "E0, E4";            //move the LEFT 4 if opponent moved the RIGHT 3
+                    } else {                    //otherwise move the RIGHT 2
+                        return "H0, F0";
+                    }
+                } else if (board[4][4] === "b4") {                     //we moved the LEFT 4 for our first move
+                    if (board[1][6] === "w3" || board[4][6] === "w3") {
+                        return "F0, F4";            //move the RIGHT 4 if opponent moved the LEFT 3
+                    } else {                    //otherwise move the LEFT 2
+                        return "C0, E0";
+                    }
+                }
+            }
+        case 3:
+            if (getMyColor() === 0) {    //we are playing white
+                if (board[5][9] === "w2") {      //we moved the RIGHT 2 for our second move
+                    return "E9, E5";            //move the LEFT 4
+                } else if (board[4][9] === "w2") { //we moved the LEFT 2 for our second move
+                    return "F9, F5";            //move the RIGHT 4
+                } else {
+                    moves = new Array("H9, F9", "C9, E9");      //let's just move a random 2 and hope for the best =)
+                    return moves[Math.floor(Math.random() * moves.length)];
+                }
+            } else {                            //we are playing black
+                if (board[5][0] === "b2") {      //we moved the RIGHT 2 for our second move
+                    return "E0, E4";            //move the LEFT 4
+                } else if (board[4][0] === "b2") { //we moved the LEFT 2 for our second move
+                    return "F0, F4";            //move the RIGHT 4
+                } else {
+                    moves = new Array("H0, F0", "C0, E0");      //let's just move a random 2 and hope for the best =)
+                    return moves[Math.floor(Math.random() * moves.length)];
+                }
+            }
+        case 4:
+            if (getMyColor() === 0) {    //we are playing white
+                if (board[5][9] === "w2") {      //we moved the RIGHT 2 already
+                    return "C9, E9";            //move the LEFT 2
+                } else {
+                    return "H9, F9";            //move the RIGHT 2
+                }
+            } else {
+                if (board[5][0] === "b2") {      //we moved the RIGHT 2 already
+                    return "C0, E0";            //move the LEFT 2
+                } else {
+                    return "H0, F0";            //move the RIGHT 2
+                }
+            }
+        default:
+            if (isPlayerInCheck(getMyColor()) === TRUE) {        // Capture the opponent’s 1
+                moves = new Array();
+                for (var i = 0; i < pieceLocations.length; i++) {
+                    var location = pieceLocations[i];
+
+                    var validMoves = getValidMoves(location, getMyColor(), null);
+                    for (var j = 0; j < validMoves.length; j++) {
+                        var move = validMoves[j];
+
+                        moves.push(location + ", " + move);
+                    }
+                }
+                if (moves.length === 0) {                //if you have no legal moves, that means you are checkmated
+                    return "CHECKMATED";
+                }
+                return moves[Math.floor(Math.random() * moves.length)];
+            } else {
+                moves = new Array();
+                for (var i = 0; i < pieceLocations.length; i++) {
+                    var location = pieceLocations[i];
+
+                    var piece = getPieceMoveDistance(location);
+                    if (piece === 4) {                   //do not move pieces that are part of the formation
+                        continue;
+                    }
+                    if (piece === 2 && cellToRow(location) % 2 !== getMyColor()) {   //neat way to check which parity of 2s
+                        continue;
+                    }
+
+                    var validMoves = getValidMoves(location, getMyColor());
+                    for (var j = 0; j < validMoves.length; j++) {
+                        var move = validMoves[j];
+
+                        moves.push(location + ", " + move);
+                    }
+                }
+                if (moves.length === 0) {                //if you have no legal moves, that means you are checkmated
+                    return moves + "     " + pieceLocations + "     " + moves.length + "  " + "CHECKMATED";
+                }
+                return moves[Math.floor(Math.random() * moves.length)];
+            }
+    }
+}
